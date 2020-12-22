f4f26cdfeb4bb1b2ba3cb13ccda7978f3fae7d77
Myra Smith <61550209+myrasmith@users.noreply.github.com>
2020-12-22 10:42:45 -0600

    Clarify expected output for multiplesToOneThousand

--- problems/5.loops.js
+++ problems/5.loops.js
@@ -7,7 +7,7 @@ function evenNumbers() {
 }
 
 /** Returns all multiples of a given number up to 1000
- * Example if num was 3 the output would be [0, 3, 6, 9, 12, ...etc ] until 1000
+ * Example if num was 3 the output would be [0, 3, 6, 9, 12, ..., 999]
 * @param {number} num - The number to find multiples of
 * @returns {number[]} - An array of all multiples of {num} up to 1000
 */
