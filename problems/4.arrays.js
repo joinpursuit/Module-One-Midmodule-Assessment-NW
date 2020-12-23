2ff35ade7d0ad72caffdef9a427c9cac5770a78f
Shoaib Dar <SDAR30@gmail.com>
2020-12-22 19:56:04 -0500

    finished assessment

--- problems/4.arrays.js
+++ problems/4.arrays.js
@@ -2,7 +2,12 @@
 * @param {number[]} arr - The input array
 * @returns {number} - The average of the first and last values
 */
-function firstAndLastAverage() {
+const firstAndLastAverage = (arr) => {
+  if (arr.length < 2) {
+    return null;
+  } else {
+    return (arr[0] + arr[arr.length - 1]) / 2
+  }
 
 }
 
@@ -12,16 +17,22 @@ function firstAndLastAverage() {
 * @param {number[]} arr - The input array
 * @returns {number[]} - The duplicated array
 */
-function duplicateArray() {
-
+function duplicateArray(arr) {
+  let arr2 = arr.concat(arr);
+  return arr2;
 }
 
 /** Removes the first 3 elements from an array. If the array has fewer than 3 elements, return an empty array.
 * @param {any[]} arr - The input array
 * @returns {any[]} - The array with the first 3 elements removed
 */
-function removeFirstThreeElements() {
-
+function removeFirstThreeElements(arr) {
+  let arr2 = []
+  if (arr.length < 3) {
+    return arr2;
+  }
+  arr2 = arr.slice(3);
+  return arr2;
 }
 
 module.exports = {
