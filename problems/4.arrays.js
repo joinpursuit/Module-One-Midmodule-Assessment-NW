b3acbc8c163ed9e16c7819d40c4970b19c7ad304
chidalgo1221 <carlos.hidalgo2017@mesacharter.org>
2020-12-22 21:25:22 -0500

    Omg I got it working

--- problems/4.arrays.js
+++ problems/4.arrays.js
@@ -2,8 +2,13 @@
 * @param {number[]} arr - The input array
 * @returns {number} - The average of the first and last values
 */
-function firstAndLastAverage() {
-
+function firstAndLastAverage(arr) {
+  if (arr.length < 2){
+    return null
+  }else{
+    averages = (arr[0] + arr[arr.length - 1]) / 2
+    return averages
+  }
 }
 
 /** Duplicates all values inside an array.
@@ -12,16 +17,23 @@ function firstAndLastAverage() {
 * @param {number[]} arr - The input array
 * @returns {number[]} - The duplicated array
 */
-function duplicateArray() {
-
+function duplicateArray(arr) {
+  let arrSecond = arr.concat(arr);
+  return arrSecond
 }
 
 /** Removes the first 3 elements from an array. If the array has fewer than 3 elements, return an empty array.
 * @param {any[]} arr - The input array
 * @returns {any[]} - The array with the first 3 elements removed
 */
-function removeFirstThreeElements() {
-
+function removeFirstThreeElements(arr) {
+  let arrSecond = []
+  if(arr.length < 3){
+    return arrSecond;
+  }else{
+    arrSecond = arr.slice(3)
+  }
+  return arrSecond
 }
 
 module.exports = {
