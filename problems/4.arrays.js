f432edcf4fc82b99ea49abd758d5ee2adb5a8de8
Demi <oluwademiladejobi@pursuit.org>
2020-12-22 21:59:40 -0500

    End of Assessment

--- problems/4.arrays.js
+++ problems/4.arrays.js
@@ -2,8 +2,12 @@
 * @param {number[]} arr - The input array
 * @returns {number} - The average of the first and last values
 */
-function firstAndLastAverage() {
-
+function firstAndLastAverage(arr) {
+  if (arr.length < 2){
+    return null
+  } else{
+    return (arr[0] + arr[arr.length - 1]) * .5
+  }
 }
 
 /** Duplicates all values inside an array.
@@ -12,16 +16,25 @@ function firstAndLastAverage() {
 * @param {number[]} arr - The input array
 * @returns {number[]} - The duplicated array
 */
-function duplicateArray() {
-
+function duplicateArray(arr) {
+  let arr1 = []
+  if(arr.length < 1){
+    return []
+  }
+  else{
+    arr1 = arr
+  }
+  return arr.concat(arr1)
 }
+
+
 
 /** Removes the first 3 elements from an array. If the array has fewer than 3 elements, return an empty array.
 * @param {any[]} arr - The input array
 * @returns {any[]} - The array with the first 3 elements removed
 */
-function removeFirstThreeElements() {
-
+function removeFirstThreeElements(arr) {
+  return arr.slice(3)
 }
 
 module.exports = {
