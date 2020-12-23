1a4353d3d11849255d3398eadea10b2ebbacf618
Jordan Bobadilla <jordanboba08@gmail.com>
2020-12-22 22:56:36 -0400

    Mid module assessment almost finished

--- problems/1.types+operators.js
+++ problems/1.types+operators.js
@@ -2,16 +2,24 @@
 * @param {any} val - The input value
 * @returns {boolean} - Is val a number
 */
-function isNumber() {
-
+const isNumber = (val) => {
+  if (typeof val === "number") {
+    return true;
+  } else {
+    return false;
+  } 
 }
 
 /** Takes in a value and returns whether it is truthy
 * @param {any} val - The input value
 * @returns {boolean} - Is val truthy
 */
-function isTruthy() {
-
+const isTruthy = (val) => {
+  if  (!val) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 
