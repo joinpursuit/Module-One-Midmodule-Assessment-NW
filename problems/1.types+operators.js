33c109f7ab32f0b0677574179fa1342d75f5eee8
D.Morales <dannellmorales@pursuit.com>
2020-12-22 21:57:23 -0500

    Module-One-Midmodule-Assessment-NW

--- problems/1.types+operators.js
+++ problems/1.types+operators.js
@@ -1,21 +1,23 @@
 /** Takes in an value and returns if it is a number
-* @param {any} val - The input value
-* @returns {boolean} - Is val a number
-*/
-function isNumber() {
-
-}
+ * @param {any} val - The input value
+ * @returns {boolean} - Is val a number
+ */
+function isNumber(num) {
+  let age = num
+    return age +7
+  }
+  console.log(typeof(isNumber(30)))
 
 /** Takes in a value and returns whether it is truthy
-* @param {any} val - The input value
-* @returns {boolean} - Is val truthy
-*/
-function isTruthy() {
-
+ * @param {any} val - The input value
+ * @returns {boolean} - Is val truthy
+ */
+function isTruthy(num) {
+  return num === num
 }
-
+console.log(isTruthy((Math.PI) === (3.14)))
 
 module.exports = {
   isNumber,
-  isTruthy
-}
+  isTruthy,
+};
--- problems/2.functions.js
+++ problems/2.functions.js
@@ -2,17 +2,21 @@
 * @param {string} name - The name of the user to greet
 * @returns {null}
 */
-function logGreeting() {
-
+function greeting(name) {
+  let str = null
+  return str
 }
+console.log(greeting())
 
 /** Returns a custom greeting string in the form "Hello Avery!"
 * @param {string} name - The name of the user to greet
 * @returns {string} - The custom greeting
 */
-function greeting() {
-
+function greeting(name) {
+  let str = "Hello"
+  return str + name
 }
+console.log(greeting("Avery"))
 
 /** Adds a number to a global variable called globalCount
  * Access globalCount like this: global.globalCount
@@ -20,18 +24,18 @@ function greeting() {
 * @returns {null}
 * @modifies {global.globalCount}
 */
-function incrementGlobalCount() {
+function incrementGlobalCount(val) {}
 
-}
 /** Multiplies three numbers
 * @param {number} firstNum - The first number
 * @param {number} secondNum - The second number
 * @param {number} thirdNum - The third number
 * @returns {number} - The product of the three numbers
 */
-function multiply() {
-
-}
+function multiply(firstNum, secondNum, thirdNum) {
+    return firstNum * secondNum * thirdNum
+  }
+  console.log(multiply(2, 2, 2))
 
 module.exports = {
   logGreeting,
