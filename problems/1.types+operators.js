21fd519520fee513ee7078d0fef13febd6a57b56
Miguel Catala <miguelcatala27@yahoo.com>
2020-12-22 21:52:12 -0500

    mod one test complete

--- problems/1.types+operators.js
+++ problems/1.types+operators.js
@@ -2,18 +2,18 @@
 * @param {any} val - The input value
 * @returns {boolean} - Is val a number
 */
-function isNumber() {
-
+function isNumber(val) {
+  return (typeof val === 'number')
 }
+  
 
 /** Takes in a value and returns whether it is truthy
 * @param {any} val - The input value
 * @returns {boolean} - Is val truthy
 */
-function isTruthy() {
-
+function isTruthy(val) {
+  return !!val;
 }
-
 
 module.exports = {
   isNumber,
