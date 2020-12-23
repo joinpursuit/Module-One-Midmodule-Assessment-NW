54a2dd46951c7a8da352541a0e9e5facf50420e0
Coreen Cooper <coreenmcooper@gmail.com>
2020-12-22 20:55:21 -0500

    solve 4.arrays.js problem 2

--- problems/4.arrays.js
+++ problems/4.arrays.js
@@ -14,8 +14,8 @@ function firstAndLastAverage(arr) {
 * @param {number[]} arr - The input array
 * @returns {number[]} - The duplicated array
 */
-function duplicateArray() {
-
+function duplicateArray(arr) {
+  return arr.concat(arr)
 }
 
 /** Removes the first 3 elements from an array. If the array has fewer than 3 elements, return an empty array.
