1bedfb8e9b6e6a245fad286359f5325a07a3f44b
Jeffrey <jeffreysebastian@pursuit.org>
2020-12-22 21:35:18 -0500

    Complete Test

--- problems/4.arrays.js
+++ problems/4.arrays.js
@@ -2,9 +2,15 @@
 * @param {number[]} arr - The input array
 * @returns {number} - The average of the first and last values
 */
-function firstAndLastAverage() {
-
-}
+const firstAndLastAverage = (arr) => {
+  if (arr.length < 2){
+    return null
+  }
+  else{
+    totalAverage = (arr[0] + arr[arr.length - 1])/2
+  }
+  return totalAverage;
+  }
 
 /** Duplicates all values inside an array.
 * Sample input: [1,4,2,5]
@@ -12,16 +18,24 @@ function firstAndLastAverage() {
 * @param {number[]} arr - The input array
 * @returns {number[]} - The duplicated array
 */
-function duplicateArray() {
-
+const duplicateArray = (arr) => {
+  let arr2 = arr.concat(arr);
+    return  arr2;
 }
 
 /** Removes the first 3 elements from an array. If the array has fewer than 3 elements, return an empty array.
 * @param {any[]} arr - The input array
 * @returns {any[]} - The array with the first 3 elements removed
 */
-function removeFirstThreeElements() {
-
+const removeFirstThreeElements = (arr) =>{
+  let arr2 = [];
+  if (arr.length < 3){
+    return arr2
+  }
+  else {
+   arr2 = arr.slice(3)
+  }
+  return arr2
 }
 
 module.exports = {
