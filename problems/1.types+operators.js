e9b0c488b559d0e2c5bc5c68afe0041444361ede
poonam <poonam398@gmail.com>
2020-12-22 20:50:21 -0500

    two problems need to finsih

--- problems/1.types+operators.js
+++ problems/1.types+operators.js
@@ -2,7 +2,12 @@
 * @param {any} val - The input value
 * @returns {boolean} - Is val a number
 */
-function isNumber() {
+function isNumber(val) {
+  if(typeof val === "number"){
+    return true
+  }else{
+    return false
+  }
 
 }
 
@@ -10,9 +15,27 @@ function isNumber() {
 * @param {any} val - The input value
 * @returns {boolean} - Is val truthy
 */
-function isTruthy() {
+function isTruthy(val) {
+  if (typeof val === "0"){
+    return false
+  }else if( typeof val === "false"){
+    return false
+  }else if(typeof val === ""){
+    return false
+  }else if(typeof val === "NaN"){
+    return false
+  }else if (typeof val === "null"){
+    return false
+  }else if (typeof val === "undefined"){
+    return false
+  }else {
+    return true
+  }
+  }
 
-}
+  console.log(isTruthy(null))
+
+
 
 
 module.exports = {
--- problems/4.arrays.js
+++ problems/4.arrays.js
@@ -2,9 +2,16 @@
 * @param {number[]} arr - The input array
 * @returns {number} - The average of the first and last values
 */
-function firstAndLastAverage() {
-
+function firstAndLastAverage(arr) {
+  if(arr.length>=2){
+    let sum = arr[0] + arr[arr.length-1]
+    let average = sum/2
+    return average 
+  }else{
+    return null 
+  }
 }
+console.log(firstAndLastAverage([2,3,4,5]))
 
 /** Duplicates all values inside an array.
 * Sample input: [1,4,2,5]
@@ -12,15 +19,22 @@ function firstAndLastAverage() {
 * @param {number[]} arr - The input array
 * @returns {number[]} - The duplicated array
 */
-function duplicateArray() {
-
+function duplicateArray(arr) {
+  let arr2=[]
+  arr.concat(arr2)
+  console.log(arr2)
 }
 
 /** Removes the first 3 elements from an array. If the array has fewer than 3 elements, return an empty array.
 * @param {any[]} arr - The input array
 * @returns {any[]} - The array with the first 3 elements removed
 */
-function removeFirstThreeElements() {
+function removeFirstThreeElements(arr) {
+  if(arr.length>=3){
+   return arr.splice(3)
+  }else{
+    return []
+  }
 
 }
 
